d5a5b9be4c1277507f4361f2f7579fd3734afa50
Neva Dinçer <nevadincer@gmail.com>
2025-02-02 01:52:56 +0300

    Added method-e

--- assign2Main.java
+++ assign2Main.java
@@ -1,13 +1,12 @@
-public class assign2Main 
 
-{
+public class assign2Main {
+
     public static final int ARRAY_SIZE = 100;
 
-    public static int getMinimumOfTheArray(int [] givenArray) {
+    public static int getMinimumOfTheArray(int[] givenArray) {
         int minimumElement = 100; // no one of the elements can be greater than 100.
         int currentElement;
-        for(int i = 0; i < assign2Main.ARRAY_SIZE; i++)
-        {
+        for (int i = 0; i < assign2Main.ARRAY_SIZE; i++) {
             currentElement = givenArray[i];
             if (currentElement <= minimumElement) {
                 minimumElement = currentElement;
@@ -16,11 +15,10 @@ public class assign2Main
         return minimumElement;
     }
 
-    public static int getMaximumOfTheArray(int [] givenArray) {
+    public static int getMaximumOfTheArray(int[] givenArray) {
         int maximumElement = 0; // no one of the elements can be less than 100.
         int currentElement;
-        for(int i = 0; i < assign2Main.ARRAY_SIZE; i++)
-        {
+        for (int i = 0; i < assign2Main.ARRAY_SIZE; i++) {
             currentElement = givenArray[i];
             if (currentElement >= maximumElement) {
                 maximumElement = currentElement;
@@ -28,13 +26,47 @@ public class assign2Main
         }
         return maximumElement;
     }
-    public static void main(String[] args) 
-    {   
+
+    public static int sumEvenIndex(int[] arr) {
+        int sum = 0;
+        int len = arr.length;
+
+        for (int i = 0; i < len; i += 2) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    public static int sumOddIndex(int[] arr) {
+        int sum = 0;
+        int len = arr.length;
+
+        for (int i = 1; i < len; i += 2) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    public static int[] sumOddAndEvenIndexes(int[] arr) {
+        int sumOdd = 0;
+        int sumEven = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                sumEven += arr[i];
+            } else {
+                sumOdd += arr[i];
+            }
+        }
+
+        return new int[]{sumEven, sumOdd};
+    }
+
+    public static void main(String[] args) {
         //an array contains random integers in the range [0,100].
-        int [] givenArray = new int[ARRAY_SIZE];
-        for(int i = 0; i < ARRAY_SIZE; i++)
-        {
-            givenArray[i] = (int)(Math.random() * 100);
+        int[] givenArray = new int[ARRAY_SIZE];
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            givenArray[i] = (int) (Math.random() * 100);
         }
         System.out.println(Arrays.toString(givenArray));
         System.out.println(Arrays.toString(DiffAverage(givenArray)));
@@ -42,6 +74,18 @@ public class assign2Main
         int minimumElement = getMinimumOfTheArray(givenArray);
         System.out.println("Maximum element of the array is " + maximumElementElement);
         System.out.println("Minimum element of the array is " + minimumElementElement);
-}
-    }    
 
+        //test sumEvenIndex
+        int evenSum = sumEvenIndex(givenArray);
+        System.out.println("\nSum of elements at even indices: " + evenSum);
+
+        //test sumOddIndex
+        int oddSum = sumOddIndex(givenArray);
+        System.out.println("Sum of elements at odd indices: " + oddSum);
+
+        //test sumOddAndEvenIndexes
+        int[] sums = sumOddAndEvenIndexes(givenArray);
+        System.out.println("\nSum of elements at even indices: " + sums[0]);
+        System.out.println("Sum of elements at odd indices: " + sums[1]);
+    }
+}
