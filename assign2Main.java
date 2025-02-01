0e9eeaaf1574f93dec9c1ba1cc18afec35c17af9
Sevval Candogan <sevvalcandogan@gmail.com>
2025-02-01 23:47:11 +0300

    Update assign2Main.java

--- assign2Main.java
+++ assign2Main.java
@@ -1,13 +1,16 @@
 public class assign2Main 
 {
+    public static final int ARRAY_SIZE = 100;
     public static void main(String[] args) 
     {   
-        final int ARRAY_SIZE = 100;
         //an array contains random integers in the range [0,100].
         int [] givenArray = new int[ARRAY_SIZE];
         for(int i = 0; i < ARRAY_SIZE; i++)
         {
             givenArray[i] = (int)(Math.random() * 100);
         }
+
+        System.out.println(Arrays.toString(givenArray));
+        System.out.println(Arrays.toString(DiffAverage(givenArray)));
     }    
 }
